f51f7c0b16648a684495010414a6024bc09a7253
NaveedIlyas <naveedilyas321@gmail.com>
2023-06-08 12:49:06 +0500

    add necessary json server files

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//! json Server deployment
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+//! json Server deployment
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
